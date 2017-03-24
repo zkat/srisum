d57283e9dfd0d70ac2eabc98230f5ea3e949d1b7
Kat Marchán <kzm@sykosomatic.org>
2017-03-24 16:37:33 -0700

    feat(cli): features implemented

--- README.md
+++ README.md
@@ -11,13 +11,21 @@
 
 ## DESCRIPTION
 
-Print or check [Subresource Integrity digests](https://w3c.github.io/webappsec/specs/subresourceintegrity/). For all its SRI handling, `srisum` leans on [`ssri`.](https://npm.im/ssri)
+Print or check Subresource Integrity digests.
+
+Spec: [](https://w3c.github.io/webappsec/specs/subresourceintegrity/)
 
 `srisum`'s API is based on the `SHA[N]SUM(1)` family of unix utilities.
 
 With no `FILE` or when `FILE` is `-`, read standard input.
 
-`-c, --check` - read SRI sums from the FILEs and check them
+`-a, --algorithms [ALGO]...` - hash algorithms to generate for the `FILE`s
+
+`-s, --strict` - follow a strict interpretation of the SRI spec
+
+`-o, --options [OPT]...` - append given `OPT` strings to generated digests
+
+`-c, --check` - read SRI sums from the `FILE`s and check them
 
 `--help` - display help and exit
 
@@ -37,9 +45,71 @@ With no `FILE` or when `FILE` is `-`, read standard input.
 
 When checking, the input should be a former output of this program. The default mode is to print line with space-separated SRI digests, one more space, and a name for each FILE.
 
-To get a better idea about how tools like `srisum` can be used for various tasks, consider reading [this blog post.](https://shapeshed.com/unix-sha1sum/)
+Strict mode, enabled with `--strict`, will entirely ignore digests (in input and output) that fail all of the following conditions:
 
-While written for `sha1sum`, the API and usage is identical. You should be able to do everything shown there with `srisum`.
+* `algorithms` must be one or more of: `sha256`, `sha384`, `sha512`
+* `options` must be visual characters except for `?`.
+* digest strings must be valid `RFC4648` `Base64` strings.
+
+## EXAMPLES
+
+### Computing SRI Digests
+
+For a single file:
+```
+$ srisum styles.css > styles.css.sri
+```
+
+For multiple different files:
+```
+$ srisum styles.css index.js package.json bundle.js > app.sri
+```
+
+From `stdin`:
+```
+$ cat styles.css | srisum -a sha1
+sha1-hmkHOZdrfLUVOqpAgryfC8XNGtE -
+```
+
+Specify algorithms to generate:
+```
+$ srisum styles.css index.js --algorithms sha512 sha256 sha1 > styles.css.sri
+```
+
+Add options:
+```
+$ srisum styles.css -a sha1 --options releaser=Kat date=2017-01-01
+sha1-hmkHOZdrfLUVOqpAgryfC8XNGtE=?releaser=kat?date=2017-01-01 styles.css
+```
+
+### Checking Integrity
+
+Passing checksum file as an argument:
+```
+$ srisum -c styles.css.sri
+styles.css: OK (sha512)
+```
+
+Passing multiple checksum files:
+```
+$ srisum -c styles.css.sri js-files.sri
+styles.css: OK (sha512)
+index.js: OK (sha512)
+lib/util.js: OK (sha512)
+```
+
+Checksum file from stdin:
+```
+$ cat styles.css.sri | srisum -c
+styles.css: OK (sha512)
+```
+
+Checksum stdin itself:
+```
+$ echo "hello" | srisum > stdin.sri
+$ echo "hello" | srisum -c stdin.sri
+-: OK (sha512)
+```
 
 ## AUTHOR
 
--- index.js
+++ index.js
@@ -1,5 +1,180 @@
 #!/usr/bin/env node
 'use strict'
 
-// const ssri = require('ssri')
-// const yargs = require('yargs')
+const fs = require('fs')
+const path = require('path')
+const split = require('split')
+const ssri = require('ssri')
+const yargs = require('yargs')
+
+main(parseArgs())
+
+function parseArgs () {
+  return yargs
+  .usage('Usage: $0 [OPTION]... [FILE...]')
+  .normalize('_')
+  .option('algorithms', {
+    alias: 'a',
+    type: 'array',
+    describe: 'hash algorithms to generate for the FILEs',
+    default: ['sha512']
+  })
+  .option('strict', {
+    alias: 's',
+    type: 'boolean',
+    describe: 'limit digests to a strict interpretation of the SRI spec'
+  })
+  .option('options', {
+    alias: 'o',
+    type: 'array',
+    describe: 'option strings to include with generated digests',
+    default: []
+  })
+  .option('check', {
+    alias: 'c',
+    type: 'boolean',
+    describe: 'read SRI checksums from the FILEs and check them'
+  })
+  .option('ignore-missing', {
+    type: 'boolean',
+    describe: 'don\'t fail or report status for missing files'
+  })
+  .option('quiet', {
+    type: 'boolean',
+    describe: 'don\'t print OK for each successfully verified file'
+  })
+  .option('status', {
+    type: 'boolean',
+    describe: 'don\'t output anything, status code shows success'
+  })
+  .option('warn', {
+    alias: 'w',
+    type: 'boolean',
+    describe: 'warn about improperly formatted checksum lines'
+  })
+  .help('help')
+  .version(() => require('./package.json').version)
+  .argv
+}
+
+function main (argv) {
+  argv._ = argv._.map(path.normalize)
+  if (argv.check) {
+    return check(argv)
+  } else {
+    return compute(argv)
+  }
+}
+
+function check (argv) {
+  const files = argv._.length ? argv._ : [null]
+  let badLines = 0
+  let badChecksums = 0
+  let missingFiles = 0
+  function outputWarnings () {
+    if (argv.status) {
+      // silence
+      return
+    }
+    if (argv.warn && badLines) {
+      console.error(`${argv.$0}: WARNING: ${badLines} line${badLines > 1 ? 's are' : ' is'} improperly formatted or invalid`)
+    }
+    if (!argv.ignoreMissing && missingFiles) {
+      console.error(`${argv.$0}: WARNING: ${missingFiles} listed file${missingFiles > 1 ? 's' : ''} could not be read`)
+    }
+    if (badChecksums) {
+      console.error(`${argv.$0}: WARNING: ${badChecksums} computed checksum${badChecksums > 1 ? 's' : ''} did NOT match`)
+    }
+  }
+  Promise.all(
+    files.map(f => {
+      return new Promise((resolve, reject) => {
+        const stream = f == null ? process.stdin : fs.createReadStream(f)
+        stream.on('error', reject)
+        const promises = []
+        stream.pipe(split(/\r?\n/, null, {trailing: false})).on('error', reject).on('data', line => {
+          const match = line.match(/^(.*)\s+(\S+)$/)
+          const integrity = match && ssri.parse(match[1], {
+            strict: argv.strict
+          })
+          if (f == null && match && match[2] === '-') {
+            promises.push(Promise.resolve(`-: FAILED`))
+          }
+          if (match && integrity.toString().length) {
+            const fileStream = match[2] === '-'
+            ? process.stdin
+            : fs.createReadStream(match[2])
+            promises.push(
+              ssri.checkStream(fileStream, integrity).then(algo => {
+                return {file: match[2], algorithm: algo}
+              }).catch(err => {
+                return {file: match[2], err}
+              })
+            )
+          } else {
+            badLines++
+          }
+        }).on('end', () => resolve(Promise.all(promises)))
+      })
+    })
+  ).catch(err => {
+    outputWarnings()
+    console.error(`${argv.$0}: ERROR: ${err.message}`)
+    process.exit(1)
+  }).then(results => {
+    !argv.status && results.forEach(lines => {
+      lines.forEach(l => {
+        if (!l.err && !argv.quiet) {
+          console.log(`${l.file}: OK (${l.algorithm})`)
+        } else if (l.err) {
+          if (l.err.code === 'EBADCHECKSUM') {
+            badChecksums++
+            console.error(`${l.file}: FAILED`)
+          } else if (l.err.code === 'ENOENT' && !argv.ignoreMissing) {
+            missingFiles++
+            console.error(`${argv.$0}: ${l.file}: No such file or directory`)
+            console.log(`${l.file}: FAILED open or read`)
+          } else {
+            console.error(`${argv.$0}: ${l.file}: ${l.err.message}`)
+            console.log(`${l.file}: FAILED ${l.err.code}`)
+          }
+        }
+      })
+    })
+    outputWarnings()
+    if (badLines || badChecksums) {
+      process.exit(1)
+    } else {
+      process.exit(0)
+    }
+  })
+}
+
+function compute (argv) {
+  const files = argv._.length ? argv._ : [null]
+  Promise.all(
+    files.map(f => {
+      const stream = f == null ? process.stdin : fs.createReadStream(f)
+      return ssri.fromStream(stream, {
+        algorithms: argv.algorithms,
+        options: argv.options,
+        strict: argv.strict
+      }).then(
+        integrity => integrity.toString().split(/\s+/).length
+        ? `${integrity} ${f || '-'}`
+        : new Error(`Valid SRI digest could not be generated for ${f}`)
+      ).catch(err => err)
+    })
+  ).then(lines => {
+    let exit = 0
+    lines.forEach(l => {
+      if (typeof l === 'string') {
+        console.log(l)
+      } else {
+        exit = 1
+        console.error(l.message)
+      }
+    })
+    process.exit(exit)
+  })
+}
