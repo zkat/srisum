9adaa55894afad11b76aeca1df03e77b38f60fb6
Kat Marchán <kzm@sykosomatic.org>
2017-03-24 02:58:52 -0700

    meta: stubbed source code files

---
+++ index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+'use strict'
+
+// const ssri = require('ssri')
+// const yargs = require('yargs')
---
+++ test/srisum.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const test = require('tap').test
+
+test('srisum does everything it needs to do')
