6825c6bd02d316a31e39ba0583507e3f1f3d89aa
Kat Marchán <kzm@sykosomatic.org>
2017-03-25 19:26:37 -0700

    test(check): stubbed check test file

---
+++ test/check.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// const BB = require('bluebird')
+
+// const execAsync = BB.promisify(require('child_process').exec, {
+//   multiArgs: true
+// })
+// const ssri = require('ssri')
+// const Tacks = require('tacks')
+const tap = require('tap')
+const test = tap.test
+
+// const CACHE = require('./util/test-dir')(__dirname)
+// const Dir = Tacks.Dir
+// const File = Tacks.File
+//
+// const srisum = 'node ' + require.resolve('..')
+
+test('checks things correctly')
